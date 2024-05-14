b274243d88322cd3cf34208440843ce1fd998417
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-05-14 16:38:07 -0300

    alterando função de copiar para função de imprimir

--- src/App.jsx
+++ src/App.jsx
@@ -25,16 +25,16 @@ function App() {
   }
 
   return (
-    <main className='flex flex-col gap-8 border p-8 rounded-lg border-gray-300 flex-wrap'>
-      <h1 className='text-3xl font-bold text-center'>Preparar pedido</h1>
-      <section className='flex gap-4 flex-wrap'>
+    <main className='flex flex-col gap-8 border p-8 rounded-lg border-gray-300 flex-wrap print:border-none'>
+      <h1 className='text-3xl font-bold text-center print:hidden'>Preparar pedido</h1>
+      <section className='flex gap-4 flex-wrap '>
     {conectado ? 
-    <div className='w-full lg:w-[400px] lg:h-[400px] border p-8 flex flex-col justify-between rounded-lg border-gray-300 gap-4'>
+    <div className='w-full lg:w-[400px] lg:h-[400px] border p-8 flex flex-col justify-between rounded-lg border-gray-300 gap-4 print:hidden'>
     <Textarea rows={10} className=' lg:h-[250px]' label='Mensagem' value={mensagem} onChange={(e) => setMensagem(e.target.value)}  />
     <Button color='purple' onClick={() => {handleClick()}}>Localizar</Button>
     </div>
     :
-    <div className='w-full lg:w-[400px] lg:h-[400px] border p-8 flex flex-col justify-between rounded-lg border-gray-300 gap-4'>
+    <div className='w-full lg:w-[400px] lg:h-[400px] border p-8 flex flex-col justify-between rounded-lg border-gray-300 gap-4 print:hidden'>
     <div className='flex flex-col gap-1'>
     <Input className='text-xs'maxLength={39} label='Informe sua chave' value={key} onChange={(e) => setKey(e.target.value)}  />
     <p className='text-xs text-gray-700'> Utilize a chave gerada pelo Google AI Studio</p>
@@ -43,13 +43,11 @@ function App() {
     </div>    
   }
 
-    <div data-color-mode="light" className='w-full lg:w-[400px] lg:h-[400px] border p-8 flex flex-col justify-between rounded-lg border-gray-300 gap-4'>
+    <div data-color-mode="light" className='w-full lg:w-[400px] lg:h-[400px] border p-8 flex flex-col justify-between rounded-lg border-gray-300 gap-4 print:border-none'>
         <MarkdownPreview style={{backgroundColor: '#f8f8ff'}} source={resposta} />
-       { resposta && <Button onClick={() => {
-        navigator.clipboard.writeText(resposta);
-        setMensagem("")
-        setResposta("")
-       }}>Copiar</Button>}
+       { resposta && <Button className='print:hidden' onClick={() => {
+        window.print()
+       }}>Imprimir</Button>}
       </div>
       </section>
     </main>
