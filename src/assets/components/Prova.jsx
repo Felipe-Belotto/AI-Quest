d7458adb9419238b5989216f0c524afdd0721c07
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-05-16 21:11:32 -0300

    adicionando feedback ao resultado

--- src/assets/components/Prova.jsx
+++ src/assets/components/Prova.jsx
@@ -39,14 +39,14 @@ export default function Prova(props) {
     <>
     {step === 0 ? (
       /* Prova  */
-  <section className='flex flex-col gap-4 bg-white p-2 pb-8'>
+  <section className='flex flex-col gap-4  p-2 pb-8'>
     <div className='p-4 flex flex-col gap-4'>
     <h1 className='text-3xl'>AI Question</h1>
     <p>Tema: {props.tema}</p>
     </div>
    
 {props.lista.map((item, index) => (
-  <div className='flex flex-col gap-4 justify-between border-gray-300 border p-4 rounded-lg lg:w-[600px]' key={index}>
+  <div className='flex flex-col gap-4 justify-between border-gray-300 bg-white border p-4 rounded-lg lg:w-[600px]' key={index}>
     <h2 className='text-xs text-[#828282] '>Questão {index + 1}</h2> 
     <h3 className='text-base '>{item.enunciado}</h3>
     <div key={index + 50} className='flex flex-col gap-2' >
@@ -65,16 +65,20 @@ export default function Prova(props) {
     </section>
     ) :
     /* Prova corrigida */
-     <section className='flex flex-col gap-4 bg-white p-2 pb-8  lg:w-[600px]'>
+     <section className='flex flex-col gap-4  p-2 pb-8  lg:w-[600px]'>
       <div className='p-4 flex flex-col gap-4'>
     <h1 className='text-3xl'>AI Question</h1>
     <p>Tema: {props.tema} (Revisão)</p>
     </div>
-    <div className='p-4 flex flex-col gap-2 border'>
-      <p className='text-3xl'>Resultado</p>
-      <div className='flex gap-8'>
-      <p className='text-xl text-green-800'>Acertos: {pontuacao} </p>
-      <p className='text-xl text-red-800'>Erros: {10 - pontuacao} </p>
+    <div className='p-4 flex items-start justify-between gap-2 border border-gray-300 bg-white lg:w-[600px]'>
+      <div className='flex flex-col gap-2'>
+      <p className='text-lg-'>Resultado</p>
+      <p className='text-xl' style={{color: pontuacao > 5 ? 'green' : 'red'}}>{pontuacao > 6 ? 'Parábens' : 'Reprovado'}</p>
+
+      </div>
+      <div className='flex gap-8 flex-col items-center gap-0 text-xl bg-[#ffffff] border border-gray-300 p-4 rounded-2xl'>
+      <p className=' text-[#555555] text-sm'>Nota </p>
+      <p style={{color: pontuacao > 5 ? 'green' : 'red'}} className='font-bold'> {pontuacao} </p>
       </div>
     </div>
     {props.lista.map((item, index) => (
